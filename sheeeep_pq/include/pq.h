d5e17c065e3a7e635bbcb24f9f784ada006163f3
Austin Yang <asd30118dd@gmail.com>
2025-12-02 01:54:06 +0800

    使用clang-format優化程式碼排版

--- sheeeep_pq/include/pq.h
+++ sheeeep_pq/include/pq.h
@@ -1,15 +1,16 @@
 #ifndef _PQ_H_
 #define _PQ_H_
-typedef int (*Compare)(const void*, const void*);
-typedef struct pq{
+typedef int (*Compare)(const void *, const void *);
+typedef struct pq
+{
     void **data;
     int capacity;
     int size;
-    int mode; //用於判定max或min堆，1為max堆，0為min堆
-}pq;
-void initpq(pq *p, int capacity,int mode);
-void push(pq *p, void *data,Compare cmp);
-void *pop(pq *p,Compare cmp);
-void deletedata(pq *p, int index,Compare cmp);
-void build_by_array(pq *p, void **data,int arrsize,Compare cmp);
+    int mode; // 用於判定max或min堆，1為max堆，0為min堆
+} pq;
+void initpq(pq *p, int capacity, int mode);
+void push(pq *p, void *data, Compare cmp);
+void *pop(pq *p, Compare cmp);
+void deletedata(pq *p, int index, Compare cmp);
+void build_by_array(pq *p, void **data, int arrsize, Compare cmp);
 #endif
--- sheeeep_pq/lib/example.c
+++ sheeeep_pq/lib/example.c
@@ -1,56 +1,64 @@
-#include "../include/pq.h"
 #include <stdio.h>
 #include <stdlib.h>
-int IntCompare(const void *a,const void *b){
-    return (*(int*)a - *(int*)b);
+
+#include "../include/pq.h"
+int IntCompare(const void *a, const void *b)
+{
+    return (*(int *)a - *(int *)b);
 }
-int main(){
-    pq p,q,array;
-    int arr[] = {5,3,8,1,4,7,2,6,4,3};
-    initpq(&p,10,1);
-    initpq(&q,10,0);
-    initpq(&array,10,1);
-    push(&p,&arr[0],IntCompare);
-    push(&p,&arr[1],IntCompare);
-    push(&p,&arr[2],IntCompare);
-    push(&p,&arr[3],IntCompare);
+int main()
+{
+    pq p, q, array;
+    int arr[] = {5, 3, 8, 1, 4, 7, 2, 6, 4, 3};
+    initpq(&p, 10, 1);
+    initpq(&q, 10, 0);
+    initpq(&array, 10, 1);
+    push(&p, &arr[0], IntCompare);
+    push(&p, &arr[1], IntCompare);
+    push(&p, &arr[2], IntCompare);
+    push(&p, &arr[3], IntCompare);
     printf("Pop elements from priority queue(max):\n");
-    while(p.size>0){
-        int *val = (int*)pop(&p,IntCompare);
-        printf("%d ",*val);
+    while (p.size > 0)
+    {
+        int *val = (int *)pop(&p, IntCompare);
+        printf("%d ", *val);
     }
-    push(&q,&arr[0],IntCompare);
-    push(&q,&arr[1],IntCompare);
-    push(&q,&arr[2],IntCompare);
-    push(&q,&arr[3],IntCompare);
+    push(&q, &arr[0], IntCompare);
+    push(&q, &arr[1], IntCompare);
+    push(&q, &arr[2], IntCompare);
+    push(&q, &arr[3], IntCompare);
     printf("\n");
     printf("Pop elements from priority queue(min):\n");
-    while(q.size>0){
-        int *val = (int*)pop(&q,IntCompare);
-        printf("%d ",*val);
+    while (q.size > 0)
+    {
+        int *val = (int *)pop(&q, IntCompare);
+        printf("%d ", *val);
     }
     printf("\n");
     initpq(&p, 10, 1);
-    push(&p,&arr[0],IntCompare);
-    push(&p,&arr[1],IntCompare);
-    push(&p,&arr[2],IntCompare);
-    push(&p,&arr[3],IntCompare);
-    deletedata(&p,1,IntCompare);
+    push(&p, &arr[0], IntCompare);
+    push(&p, &arr[1], IntCompare);
+    push(&p, &arr[2], IntCompare);
+    push(&p, &arr[3], IntCompare);
+    deletedata(&p, 1, IntCompare);
     printf("After deleting index 1(max):\n");
-    while(p.size>0){
-        int *val = (int*)pop(&p,IntCompare);
-        printf("%d ",*val);
+    while (p.size > 0)
+    {
+        int *val = (int *)pop(&p, IntCompare);
+        printf("%d ", *val);
     }
     printf("\n");
     printf("build by array(max):\n");
-    int array_size = sizeof(arr)/sizeof(int);
-    void **data_array = (void**)malloc(sizeof(void*) * array_size);
-    for(int i = 0; i < array_size; i++){
+    int array_size = sizeof(arr) / sizeof(int);
+    void **data_array = (void **)malloc(sizeof(void *) * array_size);
+    for (int i = 0; i < array_size; i++)
+    {
         data_array[i] = &arr[i];
     }
-    build_by_array(&array,data_array,array_size,IntCompare);
-    while(array.size>0){
-        int *val = (int*)pop(&array,IntCompare);
-        printf("%d ",*val);
+    build_by_array(&array, data_array, array_size, IntCompare);
+    while (array.size > 0)
+    {
+        int *val = (int *)pop(&array, IntCompare);
+        printf("%d ", *val);
     }
 }
